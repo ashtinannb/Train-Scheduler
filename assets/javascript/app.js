2963d87170464ec4c3d2cd9c10b84bb31f488693
Ashtin Brannon <ashtinannbrannon@gmail.com>
2020-05-06 21:32:13 -0500

    pushing in case i destroy

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,13 @@
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyCPrgpKbaGJYqB1RGBmayg5iHkbpMWV27Q",
-    authDomain: "train-schedule-1a0bf.firebaseapp.com",
-    databaseURL: "https://train-schedule-1a0bf.firebaseio.com",
-    projectId: "train-schedule-1a0bf",
-    storageBucket: "train-schedule-1a0bf.appspot.com",
-    messagingSenderId: "594714138763",
-    appId: "1:594714138763:web:cf2d9a9e72c790212d1c8e"
+    apiKey: "AIzaSyBmTCM_yeRjtPQWfnO_Xl3wR7xEq4sIv4I",
+    authDomain: "train-scheduler-a3432.firebaseapp.com",
+    databaseURL: "https://train-scheduler-a3432.firebaseio.com",
+    projectId: "train-scheduler-a3432",
+    storageBucket: "train-scheduler-a3432.appspot.com",
+    messagingSenderId: "777928243861",
+    appId: "1:777928243861:web:01ab56555bf98c1056f99c",
+    measurementId: "G-R9920E7WZW"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
@@ -14,17 +15,25 @@ $("#add-train").on("click", function (event) {
     event.preventDefault()
     console.log(event)
     firebase.database().ref().push({
-        username: "john",
-        email: "john@outlook.com",
+        trainName: $('#train-name').val(),
+        destination: $('#destination').val(),
+        firstTrain: $('#first-train').val(),
+        frequency: $('#frequency').val()
+
     });
-
 })
+firebase.database().ref().on('value', function (response) {
+   var dbres = response.val()
+   console.log(dbres)
+   for (let key in dbres) {
+    console.log(key)
+    console.log(dbres[key])
+    console.log(dbres[key].trainName)
+    console.log(dbres[key].destination)
+    console.log(dbres[key].firstTrain)
+    console.log(dbres[key].frequency)
+   }
 
-// firebase.database().ref().push({
-//     username: "john",
-//     email: "john@outlook.com",
-// });
-
-firebase.database().ref().on('value', function (paulo) {
-    console.log(paulo.val())
+    
 });
+
