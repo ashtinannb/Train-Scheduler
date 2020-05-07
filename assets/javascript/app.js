3919173355507730605c9737a2226f25da653e16
Ashtin Brannon <ashtinannbrannon@gmail.com>
2020-05-07 00:04:15 -0500

    submission ready

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -20,46 +20,28 @@ $("#add-train").on("click", function () {
     destination = $("#destination").val().trim();
     firstTrain = moment($("#first-train").val().trim(), "HH:mm").subtract(10, "years").format("x");
     frequency = $("#frequency").val().trim();
-
     var newTrain = {
         name: trainName,
         destination: destination,
         firstTrain: firstTrain,
         frequency: frequency
     }
-
     dbres.ref().push(newTrain);
-
-    alert("train added");
-
+    alert("You added a train!!! :D ");
     $('#train-name').val("");
     $("#destination").val("");
     $("#first-train").val("");
     ("#frequency").val("");
-
-    alert("train added");
-
 });
 
-
-
-
-
-dbres.ref().on("child_added",function(snapshot) {
+dbres.ref().on("child_added", function (snapshot) {
     var name = snapshot.val().name;
     var destination = snapshot.val().destination;
     var frequency = snapshot.val().frequency;
     var firstTrain = snapshot.val().firstTrain;
-
     var remainder = moment().diff(moment.unix(firstTrain), "minutes") % frequency;
     var minutes = frequency - remainder;
-    var arrival = moment().add(minutes,"m").format("hh:mm A");
-
-    console.log(remainder);
-    console.log(minutes);
-    console.log(arrival);
-
-    $(".table > tBody").append("<tr><td>" + name + "</td><td>" + destination + "</td><td>" +frequency+"</td><td>" + arrival + "</td><td>" + minutes + "</td></tr>");
+    var arrival = moment().add(minutes, "m").format("hh:mm A");
+    $(".table > tBody").append("<tr><td>" + name + "</td><td>" + destination + "</td><td>" + frequency + "</td><td>" + arrival + "</td><td>" + minutes + "</td></tr>");
 }
 )
-//
